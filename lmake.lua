38b4bfad2b4801b31f4bd3a2eca0c6284784c81c
xiyoo0812 <xiyoo0812@163.com>
2021-09-16 11:57:20 +0800

    使用lstdfs替换lfs

--- lmake.lua
+++ lmake.lua
@@ -1,23 +1,21 @@
 --lmake.lua
-local lfs           = require("lfs")
+local lstdfs        = require('lstdfs')
 
-local ldir          = lfs.dir
-local lcurdir       = lfs.currentdir
-local lattributes   = lfs.attributes
-local ssub          = string.sub
+local ldir          = lstdfs.dir
+local lstem         = lstdfs.stem
+local lappend       = lstdfs.append
+local lconcat       = lstdfs.concat
+local labsolute     = lstdfs.absolute
+local lextension    = lstdfs.extension
+local lcurdir       = lstdfs.current_path
+local lrelativedir  = lstdfs.relative_path
+local lrepextension = lstdfs.replace_extension
 local sgsub         = string.gsub
-local sfind         = string.find
 local sformat       = string.format
-local smatch        = string.match
-local sgmatch       = string.gmatch
 local tinsert       = table.insert
 local tsort         = table.sort
 
-local slash         = "/"
 local projects      = {}
-local makefiles     = {}
-local work_dir      = lfs.currentdir() .. slash
-local main_make     = work_dir .. "lmake"
 
 --table包含
 local function tcontain(tab, val)
@@ -45,8 +43,23 @@ local function group_sort(a, b)
     return a.INDEX < b.INDEX
 end
 
+--
+local function path_fmt(paths)
+    for i, path in pairs(paths) do
+        paths[i] = sgsub(path, "/", "\\")
+    end
+    return paths
+end
+
+--路径剪裁
+local function path_cut(fullname, basename)
+    local cutname = sgsub(fullname, basename, "")
+    local relapath = lrelativedir(cutname)
+    return relapath
+end
+
 --初始化solution环境变量
-local function init_solution_env(env, projects)
+local function init_solution_env(env)
     local groups = {}
     local fmt_groups = ""
     tsort(projects, project_sort)
@@ -67,66 +80,26 @@ local function init_solution_env(env, projects)
     env.GROUPS = groups
 end
 
---获取路径
-local function get_file_path(filename)
-    return smatch(filename, "(.+)/[^/]*%.%w+$")
-end
-
---获取文件名
-local function get_file_name(filename)
-    return smatch(filename, ".+/([^/]*%.%w+)$")
-end
-
---获取文件名
-local function get_file_title(filename)
-    return smatch(filename, ".+/([^/]*)(%.%w+)$")
-end
-
---去除扩展名
-local function get_file_root(filename)
-    local idx = smatch(filename, ".+()%.%w+$")
-    if idx then
-        return ssub(filename, 1, idx-1)
-    end
-    return filename
-end
-
---获取扩展名
-local function get_file_ext(filename)
-    return smatch(filename, ".+%.(%w+)$")
-end
-
---是否lmak文件
-local function is_lmak_file(filename)
-    if get_file_ext(filename) == "lmak" then
-        return true
-    end
-    return false
-end
-
 --收集文件
-local function collect_files(collect_dir, root_dir, source_dir, args, group, collects, is_hfile)
-    for file in ldir(collect_dir) do
-        if file == "." or file == ".." then
+local function collect_files(collect_dir, project_dir, source_dir, args, group, collects, is_hfile)
+    local dir_files = ldir(collect_dir)
+    for _, file in pairs(dir_files) do
+        local fullname = file.name
+        if file.type ~= "regular" then
             goto continue
         end
-        local filename = collect_dir .. file
-        local attr = lattributes(filename)
-        if attr.mode ~= "file" then
-            goto continue
-        end
-        local ext_name = get_file_ext(filename)
-        local fmt_name = sgsub(filename, root_dir, "")
+        local ext_name = lextension(fullname)
+        local fmt_name = path_cut(fullname, project_dir)
         if is_hfile then
-            if ext_name == "h" then
+            if ext_name == ".h" then
                 tinsert(collects, {fmt_name, group, false, false})
             end
             goto continue
         end
-        if ext_name == "c" or ext_name == "cc" or ext_name == "cpp" then
-            local cmp_name = sgsub(filename, source_dir, "")
+        if ext_name == ".c" or ext_name == ".cc" or ext_name == ".cpp" then
+            local cmp_name = path_cut(fullname, source_dir)
             local is_obj = tcontain(args.OBJS, cmp_name)
-            local is_exclude = tcontain(args.EXCLUDE_FILE, cmp_name)
+            local is_exclude = tcontain(path_fmt(args.EXCLUDE_FILE), cmp_name)
             tinsert(collects, {fmt_name, group, is_exclude, is_obj})
         end
         :: continue ::
@@ -134,24 +107,23 @@ local function collect_files(collect_dir, root_dir, source_dir, args, group, col
 end
 
 --vs工程收集源文件
-local function collect_sources(proj_dir, source_dir, args)
+local function collect_sources(project_dir, src_dir, args)
     local includes, sources = {}, {}
-    local root_dir = proj_dir .. slash
-    local source_dir = root_dir .. source_dir .. slash
-    collect_files(source_dir, root_dir, source_dir, args, "inc", includes, true)
-    collect_files(source_dir, root_dir, source_dir, args, "src", sources, false)
+    local source_dir = lappend(project_dir, src_dir)
+    collect_files(source_dir, project_dir, source_dir, args, "inc", includes, true)
+    collect_files(source_dir, project_dir, source_dir, args, "src", sources, false)
     for _, sub_dir in ipairs(args.SUB_DIR) do
-        collect_files(source_dir .. sub_dir .. slash, root_dir, source_dir, args, sub_dir, includes, true)
-        collect_files(source_dir .. sub_dir .. slash, root_dir, source_dir, args, sub_dir, sources, false)
+        collect_files(lappend(source_dir, sub_dir), project_dir, source_dir, args, sub_dir, includes, true)
+        collect_files(lappend(source_dir, sub_dir), project_dir, source_dir, args, sub_dir, sources, false)
     end
     return includes, sources
 end
 
 --初始化项目环境变量
-local function init_project_env(proj_dir, bmimalloc)
+local function init_project_env(project_dir, bmimalloc)
     local lguid = require("lguid")
     return {
-        WORK_DIR    = proj_dir,
+        WORK_DIR    = project_dir,
         GUID_NEW    = lguid.guid,
         MIMALLOC    = bmimalloc,
         COLLECT     = collect_sources,
@@ -174,38 +146,34 @@ local function load_env_file(file, env)
 end
 
 --生成项目文件
---proj_dir：项目目录
+--project_dir：项目目录
 --lmake_dir：项目目录相对于lmake的路径
-local function build_projfile(proj_dir, lmake_dir, bmimalloc)
+local function build_projfile(solution_dir, project_dir, lmake_dir, bmimalloc)
     local lguid = require("lguid")
     local ltmpl = require("ltemplate.ltemplate")
-    for file in ldir(proj_dir) do
-        if file == "." or file == ".." then
+    local dir_files = ldir(project_dir)
+    for _, file in pairs(dir_files) do
+        local fullname = file.name
+        if file.type == "directory" then
+            build_projfile(solution_dir, fullname, lmake_dir, bmimalloc)
             goto continue
         end
-        local file_name = proj_dir .. slash .. file
-        local attr = lattributes(file_name)
-        if attr.mode == "directory" then
-            build_projfile(file_name, lmake_dir, bmimalloc)
-            goto continue
-        end
-        if is_lmak_file(file_name) then
-            local env = init_project_env(proj_dir, bmimalloc)
-            local mak_dir = sgsub(proj_dir, work_dir, "")
-            local file_root = get_file_root(file_name)
-            if not load_env_file(lmake_dir .. slash .. "share.lua", env) then
+        if lextension(fullname) == ".lmak" then
+            local env = init_project_env(project_dir, bmimalloc)
+            if not load_env_file(lappend(lmake_dir, "share.lua"), env) then
                 error("load share lmake file failed")
                 return
             end
-            ltmpl.render_file(lmake_dir .. slash .. "tmpl/make.tpl", file_root .. ".mak", env, file_name)
-            ltmpl.render_file(lmake_dir .. slash .. "tmpl/vcxproj.tpl", file_root .. ".vcxproj", env, file_name)
-            ltmpl.render_file(lmake_dir .. slash .. "tmpl/filters.tpl", file_root .. ".vcxproj.filters", env, file_name)
+            local mak_dir = path_cut(project_dir, solution_dir)
+            ltmpl.render_file(lappend(lmake_dir, "tmpl/make.tpl"),  lrepextension(fullname, ".mak"), env, fullname)
+            ltmpl.render_file(lappend(lmake_dir, "tmpl/vcxproj.tpl"),  lrepextension(fullname, ".vcxproj"), env, fullname)
+            ltmpl.render_file(lappend(lmake_dir, "tmpl/filters.tpl"),  lrepextension(fullname, ".vcxproj.filters"), env, fullname)
             tinsert(projects, {
                 DIR = mak_dir,
                 DEPS = env.DEPS,
                 GROUP = env.GROUP,
                 NAME = env.PROJECT_NAME,
-                FILE = get_file_title(file_name),
+                FILE = lstem(fullname),
                 GUID = lguid.guid(env.PROJECT_NAME)
             })
         end
@@ -214,38 +182,44 @@ local function build_projfile(proj_dir, lmake_dir, bmimalloc)
 end
 
 --生成项目文件
-local function build_lmak()
+local function build_lmak(solution_dir)
     local env = {}
-    if not load_env_file(main_make, env) then
+    print(sformat("build_lmak: solution_dir %s", solution_dir))
+    if not load_env_file(lappend(solution_dir, "lmake"), env) then
         error("load main lmake file failed")
         return
     end
-    local solution, lmake_dir = env.SOLUTION, env.LMAKE_DIR
-    if not solution or not lmake_dir then
+    local solution = env.SOLUTION
+    if not solution or not env.LMAKE_DIR then
         error(sformat("lmake solution or dir not config"))
         return
     end
-    package.path = sformat("%s;%s/?.lua", package.path, env.LMAKE_DIR)
-    for file in ldir(work_dir) do
-        if file == "." or file == ".." then
-            goto continue
+    local lmake_dir = labsolute(lappend(solution_dir, env.LMAKE_DIR))
+    print(sformat("build_lmak: lmake_dir %s", lmake_dir))
+    package.path = sformat("%s;%s/?.lua", package.path, lmake_dir)
+    local dir_files = ldir(solution_dir)
+    for _, file in pairs(dir_files) do
+        if file.type == "directory" then
+            build_projfile(solution_dir, file.name, lmake_dir, env.USE_MIMALLOC)
         end
-        local sub_name = work_dir .. file
-        local attr = lattributes(sub_name)
-        if attr.mode == "directory" then
-            build_projfile(sub_name, lmake_dir, env.USE_MIMALLOC)
-        end
-        :: continue ::
     end
-    init_solution_env(env, projects)
+    init_solution_env(env)
     local ltmpl = require("ltemplate.ltemplate")
-    ltmpl.render_file(lmake_dir .. slash .. "tmpl/makefile.tpl", "Makefile", env)
-    ltmpl.render_file(lmake_dir .. slash .. "tmpl/Solution.tpl", solution .. ".sln", env)
+    ltmpl.render_file(lappend(lmake_dir, "tmpl/makefile.tpl"), lappend(solution_dir, "Makefile"), env)
+    ltmpl.render_file(lappend(lmake_dir, "tmpl/Solution.tpl"), lappend(solution_dir, lconcat(solution, ".sln")), env)
     print(sformat("build solution %s success!", solution))
 end
 
---工具用法
-build_lmak()
+local solution_dir = lcurdir()
+if select("#", ...) == 1 then
+    solution_dir = select(1, ...)
+end
+--生成工程
+--solution_dir：工程路径
+build_lmak(labsolute(solution_dir))
 
+--工具用法
 --usage
---bin/lua.exe ../lmake/lmake.lua
+--lmake_file: lmake.lua文件路径
+--solution_dir：工程路径，不传则当前路径
+--bin/lua.exe ../lmake/lmake.lua ./
